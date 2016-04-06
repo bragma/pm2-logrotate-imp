eec33e1399ced3593406a613187d0f978fbbf015
Marco Braga <marco.braga@gmail.com>
2016-04-07 01:02:31 +0200

    Support combined log files. Don't rotate combine instance logs more than once per pass.

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ function retainOrDeleteFiles(file, retain) {
 			// Sort them by name - This may be totally incorrect if the date is not sortable
 			// Reverse the sorting order
 			var rotated_files = files.filter(function(file) {
-				return fileBaseName === (readPath + file).substr(0, fileBaseName.length); 
+				return fileBaseName === (readPath + file).substr(0, fileBaseName.length);
 			}).sort().reverse();
 
 			// Retain the specified amount of files
@@ -63,7 +63,7 @@ function retainOrDeleteFiles(file, retain) {
 
 			// Delete all files
 			return Promise.map(to_delete, function(file) {
-				file = readPath + file; 
+				file = readPath + file;
 				return fs.unlinkAsync(file)
 					.then(function() {
 						console.log('"' + file + '" has been deleted');
@@ -71,7 +71,7 @@ function retainOrDeleteFiles(file, retain) {
 					.catch(function(err) {
 						console.error(err.stack || err);
 					});
-			});		
+			});
 		})
 		.catch(function(err) {
 			console.error(err.stack || err);
@@ -80,20 +80,20 @@ function retainOrDeleteFiles(file, retain) {
 
 
 function promisePipe(source, sink) {
-	var resolve, reject; 
-	return new Promise(function(resolve_, reject_) { 
-		resolve = resolve_; 
-		reject = reject_; 
-		source 
-			.on("end", resolve) 
-			.on("error", reject) 
-			.pipe(sink) 
-			.on("error", reject); 
-		}).finally(function() { 
-			source.removeListener("end", resolve); 
-			source.removeListener("error", reject); 
-			sink.removeListener("error", reject); 
-		}); 
+	var resolve, reject;
+	return new Promise(function(resolve_, reject_) {
+		resolve = resolve_;
+		reject = reject_;
+		source
+			.on("end", resolve)
+			.on("error", reject)
+			.pipe(sink)
+			.on("error", reject);
+	}).finally(function() {
+		source.removeListener("end", resolve);
+		source.removeListener("error", reject);
+		sink.removeListener("error", reject);
+	});
 }
 
 function processFile(file, force, conf) {
@@ -106,14 +106,14 @@ function processFile(file, force, conf) {
 	return (conf.rotation_mode === 'reload'
 		? fs.renameAsync(file, final_name)
 		: promisePipe(
-				fs.createReadStream(file),
-				fs.createWriteStream(final_name, { 'flags': 'a' })
-			).then(function() {
-				return fs.truncateAsync(file, 0);
-			})
-		)
+			fs.createReadStream(file),
+			fs.createWriteStream(final_name, { 'flags': 'a' })
+		).then(function() {
+			return fs.truncateAsync(file, 0);
+		})
+	)
 		.then(function() {
-			console.log('"' + final_name + '" has been created ('+ (force ? 'time' : 'size') +' exceeded)');
+			console.log('"' + final_name + '" has been created (' + (force ? 'time' : 'size') + ' exceeded)');
 		})
 		.catch(function(err) {
 			console.error(err.stack || err);
@@ -128,13 +128,14 @@ function processFile(file, force, conf) {
 }
 
 function checkAndRotate(file, force, conf) {
-	
+
 	return fs.statAsync(file)
 		.then(function(stat) {
-			gl_file_list.push(file);
-			
-			if (force || stat.size >= conf.max_size) {
-				return processFile(file, force, conf);
+			if (gl_file_list.indexOf(file) === -1) {
+				gl_file_list.push(file);
+				if (force || stat.size >= conf.max_size) {
+					return processFile(file, force, conf);
+				}
 			}
 		})
 		.catch(function(err) {
@@ -146,14 +147,26 @@ function checkAndRotate(file, force, conf) {
 }
 
 function checkAndRotateAppfiles(app, force, conf) {
-	// Get error and out file
-	var out_file = app.pm2_env.pm_out_log_path;
-	var err_file = app.pm2_env.pm_err_log_path;
 
-	return Promise.join(
-		checkAndRotate(out_file, force, conf),
-		checkAndRotate(err_file, force, conf)
-	);
+	var files = [];
+
+	if (app.pm2_env.pm_out_log_path) {
+		files.push(path.normalize(app.pm2_env.pm_out_log_path));
+	}
+
+	if (app.pm2_env.pm_err_log_path) {
+		files.push(path.normalize(app.pm2_env.pm_err_log_path));
+	}
+
+	if (app.pm2_env.pm_log_path) {
+		files.push(path.normalize(app.pm2_env.pm_log_path));
+	}
+
+	return Promise.map(files.filter(function(item, pos, self) {
+		return self.indexOf(item) == pos;
+	}), function(file) {
+		return checkAndRotate(file, force, conf);
+	});
 }
 
 function is_it_time_yet(conf) {
@@ -171,22 +184,21 @@ function is_it_time_yet(conf) {
 // Connect to local PM2
 pm2connectAsync()
 	.then(function() {
-		
+
 		function worker() {
 			// Get process list managed by PM2
 			pm2listAsync()
 				.then(function(apps) {
 					var force = is_it_time_yet(currentConfig);
-					
-					return Promise.map(apps, function (app) {
-							return checkAndRotateAppfiles(app, force, currentConfig);
-						})
+					return Promise.map(apps, function(app) {
+						return checkAndRotateAppfiles(app, force, currentConfig);
+					})
 						.finally(function() {
-							
+
 							// HACK: Since pm2 does not reload its own logs, we have to fallback
 							// to copytruncate method.
 							// See: https://github.com/Unitech/pm2/issues/800
-							
+
 							var conf = util._extend({}, currentConfig);
 							conf.rotation_mode = "copytruncate";
 
@@ -205,20 +217,21 @@ pm2connectAsync()
 					console.error(err.stack || err);
 				});
 		}
-	
-		setTimeout(function () {
-			setInterval(function () {
+
+		setTimeout(function() {
+			setInterval(function() {
 				gl_file_list = [];
 				worker();
 			}, WORKER_INTERVAL);
 		}, (WORKER_INTERVAL - (Date.now() % WORKER_INTERVAL)));
-		
-		
+
+
 	})
 	.catch(function(err) {
 		console.error(err.stack || err);
 	});
 
-pmx.action('list files', function (reply) {
+// Return log files when keymetrix asks the list
+pmx.action('list files', function(reply) {
 	return reply(gl_file_list);
 });
